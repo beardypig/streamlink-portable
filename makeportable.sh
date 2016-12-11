3e81d21479f2f75fcf293a2274794dfc1e9ab7ab
beardypig <beardypig@users.noreply.github.com>
2016-12-11 21:42:17 +0000

    couple of tweaks to the layout, and include the Windows 10 Universal C Runtime, fixes #1

--- makeportable.sh
+++ makeportable.sh
@@ -3,7 +3,7 @@
 set -e # quit on error
 
 STREAMLINK_PYTHON_ARCH=${1:-win32}
-STREAMLINK_PYTHON_VERSION=${STREAMLINK_PYTHON_VERSION:-3.5.2}
+STREAMLINK_PYTHON_VERSION=3.5.2
 PYTHON_PLATFORM=${STREAMLINK_PYTHON_ARCH}
 
 if [[ $STREAMLINK_PYTHON_ARCH == "amd64" ]]; then
@@ -16,13 +16,14 @@ root_dir="$(pwd)"
 temp_dir="${root_dir}/build/temp"
 bundle_dir="${temp_dir}/streamlink"
 python_dir="${bundle_dir}/python"
+packages_dir="${bundle_dir}/packages"
 streamlink_clone_dir="${temp_dir}/streamlink-clone"
 dist_dir="${root_dir}/dist"
 
 mkdir -p "${bundle_dir}"
 mkdir -p "${dist_dir}"
 
-wget "${python_url}" -O "build/temp/python-${STREAMLINK_PYTHON_VERSION}-embed-${STREAMLINK_PYTHON_ARCH}.zip"
+wget "${python_url}" -c -O "build/temp/python-${STREAMLINK_PYTHON_VERSION}-embed-${STREAMLINK_PYTHON_ARCH}.zip"
 
 # remove any old streamlink clone
 rm -rf "${streamlink_clone_dir}"
@@ -31,7 +32,7 @@ git clone https://github.com/streamlink/streamlink.git ${streamlink_clone_dir}
 pushd "${streamlink_clone_dir}"
 commit=$(git rev-parse --short HEAD)
 
-pip download --only-binary ":all:" --platform "${PYTHON_PLATFORM}" --python-version "35" -d "${temp_dir}" "pycryptodome==3.4.3" "requests>=1.0,<2.12.0"
+pip download --only-binary ":all:" --platform "${PYTHON_PLATFORM}" --python-version "35" --abi "cp35m" -d "${temp_dir}" "pycryptodome==3.4.3" "requests>=1.0,!=2.12.0,!=2.12.1,<3.0"
 
 # Work out the streamlink version
 # For travis nightly builds generate a version number with commit hash
@@ -43,16 +44,21 @@ popd
 env NO_DEPS=1 pip wheel streamlink -w "${temp_dir}"
 
 unzip -o "build/temp/python-${STREAMLINK_PYTHON_VERSION}-embed-${STREAMLINK_PYTHON_ARCH}.zip" -d "${python_dir}"
-unzip -o "build/temp/streamlink*.whl" -d "${python_dir}"
-unzip -o "build/temp/pycryptodome*.whl" -d "${python_dir}"
-unzip -o "build/temp/requests*.whl" -d "${python_dir}"
+# include the Windows 10 Universal Runtime
+unzip -o "msvcrt_x86.zip" -d "${python_dir}"
+
+unzip -o "build/temp/streamlink*.whl" -d "${packages_dir}"
+unzip -o "build/temp/pycryptodome*.whl" -d "${packages_dir}"
+unzip -o "build/temp/requests*.whl" -d "${packages_dir}"
 
 cp "${root_dir}/streamlink-script.py" "${bundle_dir}/streamlink-script.py"
 cp "${root_dir}/streamlink.bat" "${bundle_dir}/streamlink.bat"
+cp "${root_dir}/NOTICE" "${bundle_dir}/NOTICE.txt"
 
 mkdir -p "${bundle_dir}/rtmpdump"
 cp -r "${streamlink_clone_dir}/win32/rtmpdump/"* "${bundle_dir}/rtmpdump"
 cp -r "${streamlink_clone_dir}/win32/streamlinkrc" "${bundle_dir}/streamlinkrc.default"
+cp -r "${streamlink_clone_dir}/win32/LICENSE.txt" "${bundle_dir}/LICENSE.txt"
 
 sed -i "s/^rtmpdump=.*/rtmpdump=rtmpdump\\\\rtmpdump.exe/g" "${bundle_dir}/streamlinkrc.default"
 
--- streamlink-script.py
+++ streamlink-script.py
@@ -1,7 +1,16 @@
-import os.path
+#!python
 import shutil
-from streamlink_cli.main import main
-# install the streamlinkrc file, if one is not installed
-if not os.path.exists("streamlinkrc"):
-  shutil.copyfile("streamlinkrc.default", "streamlinkrc")
-main()
+import sys
+import os.path
+
+pkgdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'packages'))
+sys.path.insert(0, pkgdir)
+os.environ['PYTHONPATH'] = os.pathsep.join([pkgdir, os.environ.get('PYTHONPATH', '')])
+
+if __name__ == '__main__':
+    from streamlink_cli.main import main
+
+    # install the streamlinkrc file, if one is not installed
+    if not os.path.exists("streamlinkrc"):
+        shutil.copyfile("streamlinkrc.default", "streamlinkrc")
+    main()
